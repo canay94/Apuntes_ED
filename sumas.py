a73527ddb7ae8913a474b14ae69a0eb35ddba031
Alejandro César Rico Martínez <alejandro.rico@tecsuperiorslp.edu.mx>
2021-10-08 20:59:28 -0500

    Archivo README.md y sumas.py

---
+++ sumas.py
@@ -0,0 +1,20 @@
+# programa en Python para sumar dos numeros
+# si no se declara el tipo del dato, lo toma como string
+x = input("Ingrese el valor de x: ")
+y = input("Ingrese el valor de y: ")
+resultado = x + y
+print("El resultado de la suma es: ", resultado)
+
+# si se declara el tipo del dato, lo toma como int
+x = int(input("Ingrese el valor de x: "))
+y = int(input("Ingrese el valor de y: "))
+resultado = x + y
+print("El resultado de la suma es: ", resultado)
+
+# si se declara el tipo del dato, lo toma como float
+# con este tipo de dato se pueden sumar numeros enteros (int) y flotantes (float)
+x = float(input("Ingrese el valor de x: "))
+y = float(input("Ingrese el valor de y: "))
+resultado = x + y
+print("El resultado de la suma es: ", resultado)
+
